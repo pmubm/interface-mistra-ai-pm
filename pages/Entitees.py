bace26531f555598582c1479ac8e267eda9c318f
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 11:01:48 +0100

    Update Entitees.py

--- pages/Entitees.py
+++ pages/Entitees.py
@@ -6,9 +6,8 @@ st.title('Entitees')
 #ay4EXIYW5M1jqCtssLjyzRnZjkKwbA5f
 api_keys =st.text_input("api_keys")
 client = Mistral(api_keys)
-
-prompt = st.text_area("""
-            Le "meilleur" fromage français est une question subjective qui dépend des goûts personnels de chacun. La France est célèbre pour sa grande variété de fromages, chacun ayant ses propres caractéristiques uniques en termes de texture, de saveur et de méthode de fabrication. Voici quelques fromages français très appréciés parmi les amateurs de fromage :
+texte_par_defaut = """
+ Le "meilleur" fromage français est une question subjective qui dépend des goûts personnels de chacun. La France est célèbre pour sa grande variété de fromages, chacun ayant ses propres caractéristiques uniques en termes de texture, de saveur et de méthode de fabrication. Voici quelques fromages français très appréciés parmi les amateurs de fromage :
 
 1. **Camembert de Normandie** : Un fromage à pâte molle et à croûte fleurie, originaire de Normandie, avec une texture crémeuse et une saveur douce mais complexe.
 2. **Roquefort** : Un fromage persillé (à pâte bleue) fabriqué à partir de lait de brebis, avec une saveur forte et piquante.
@@ -19,9 +18,11 @@ prompt = st.text_area("""
 7. **Munster** : Un fromage à pâte molle et à croûte lavée, originaire des Vosges, avec une texture crémeuse et une saveur forte et aromatique.
 8. **Époisses** : Un fromage à pâte molle et à croûte lavée, originaire de Bourgogne, avec une texture crémeuse et une saveur forte et épicée.
 
-Chacun de ces fromages a ses propres qualités et peut être considéré comme le "meilleur" en fonction des préférences individuelles. La diversité des fromages français permet de satisfaire une grande variété de goûts et de palais.          
-                      
-""")
+Chacun de ces fromages a ses propres qualités et peut être considéré comme le "meilleur" en fonction des préférences individuelles. La diversité des fromages français permet de satisfaire une grande variété de goûts et de palais. 
+"""
+
+
+prompt = st.text_area("Votre texte :", value=texte_par_defaut)
 
 if st.button("Envoyer"):
     response = get_ner(client,prompt)
