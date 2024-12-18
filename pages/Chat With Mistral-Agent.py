0863a789c12616ef3cc703384890481d57a4abf4
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 14:50:21 +0100

    Update Chat With Mistral-Agent.py

--- pages/Chat With Mistral-Agent.py
+++ pages/Chat With Mistral-Agent.py
@@ -47,20 +47,3 @@ elif selection == "Entitees":
 
 
 
-
-'''
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    
-    response = get_ner(client, prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
-'''
