a0e10d307aad2d27c2cfa767d1ebcbbdc097376a
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 09:54:47 +0100

    Update home.py

--- home.py
+++ home.py
@@ -42,5 +42,12 @@ user_age = st.slider("Quel est votre age ?", 18, 99, 30)
 st.selectbox("Selectionnez votre pays", ["France", "Espagen", "USA"])
 
 
+
 # Lecture d'un fichier csv avec pandas
-#https://raw.githubusercontent.com/Quera-fr/My-Credit/refs/heads/main/Analyse%20des%20donn%C3%A9es/test.csv
+import pandas as pd
+
+path_url = "https://raw.githubusercontent.com/Quera-fr/My-Credit/refs/heads/main/Analyse%20des%20donn%C3%A9es/test.csv"
+
+df = pd.read_csv(path_url)
+
+st.write(df, delimiter=';')
