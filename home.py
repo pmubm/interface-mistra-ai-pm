9deb7627519d4397a65d2e89a246000d5949fc03
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 09:24:39 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.title('interface-mistral-ai-pm')
+
+
+st.subheader("Mistral AI")
+
+
+st.write("Introduction à mistral AI"
+
+st.write("""
+# Titre
+
+## Sous titre
+ `print("Hello World")`
+""")
+
+         
+# Creation d'un bouton
+
+        
+         
