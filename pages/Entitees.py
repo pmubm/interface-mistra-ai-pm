8ea51d87c325b73fbb0e613bc2ecdcddfb82b181
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 11:17:23 +0100

    Update Entitees.py

--- pages/Entitees.py
+++ pages/Entitees.py
@@ -8,11 +8,7 @@ chaine_apik = "ay4EXIYW5M1jqCtssLjyzRnZjkKwbA5f"
 text_area_api =st.text_input("api_keys",value=chaine_apik)
 
 client = Mistral(text_area_api)
-texte_par_defaut = """Bien sûr. Voici une version du texte d'entraînement sans apostrophes, toujours sans chiffres ni guillemets, pour faciliter encore davantage le traitement par un modèle de NER :
-
-Texte d'entraînement (version courte, sans chiffres, guillemets ni apostrophes) :
-
-Emmanuel Macron a rencontré Angela Merkel à Berlin. La discussion portait sur lavenir de lUnion Européenne. Macron sest ensuite rendu à New York pour une session de lAssemblée générale des Nations Unies. Elon Musk dirigeant de Tesla a annoncé une collaboration avec Samsung. Joe Biden président des États-Unis a effectué un voyage à Kyiv. Marie Curie scientifique de renom est née à Varsovie. Albert Einstein a travaillé à Berne. Microsoft fondée par Bill Gates et Paul Allen a lancé une nouvelle version de Windows. Le tournoi de Roland Garros se déroule chaque année à Paris. Narendra Modi Premier ministre de lInde a visité Paris. LOrganisation mondiale de la Santé OMS basée à Genève surveille les épidémies mondiales
+texte_par_defaut = """ Joe Biden président des États-Unis a effectué un voyage à Kyiv. Marie Curie scientifique de renom est née à Varsovie. Albert Einstein a travaillé à Berne. Microsoft fondée par Bill Gates et Paul Allen a lancé une nouvelle version de Windows. Le tournoi de Roland Garros se déroule chaque année à Paris. Narendra Modi Premier ministre de lInde a visité Paris. LOrganisation mondiale de la Santé OMS basée à Genève surveille les épidémies mondiales
 """
 # Affiche le contenu actuel de la boîte de texte
 prompt = st.text_area("Votre texte :", value=texte_par_defaut)
