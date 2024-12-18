c8129d31e824ba1362679051f96ae5cf029e9c49
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 11:13:27 +0100

    Update Entitees.py

--- pages/Entitees.py
+++ pages/Entitees.py
@@ -8,8 +8,8 @@ chaine_apik = "ay4EXIYW5M1jqCtssLjyzRnZjkKwbA5f"
 text_area_api =st.text_input("api_keys",value=chaine_apik)
 
 client = Mistral(text_area_api)
-texte_par_defaut = """
-Plus tard dans le mois, le 27 juillet, Elon Musk, PDG de Tesla, a annoncé un nouveau partenariat avec Samsung pour la production de batteries. En 2022, Joe Biden, le président des États-Unis, a visité Kyiv en Février. Marie Curie, une scientifique renommée, est née à Varsovie le 7 novembre 1867. Albert Einstein a travaillé à l'Office des brevets à Berne au début du XXe siècle.
+texte_par_defaut = """par Bill Gates et Paul Allen, a lancé Windows
+
 """
 # Affiche le contenu actuel de la boîte de texte
 prompt = st.text_area("Votre texte :", value=texte_par_defaut)
