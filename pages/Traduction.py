503585732f88d8554ae5cc6218e7a6fb870689b1
pmubm <48210824+pmubm@users.noreply.github.com>
2024-12-18 10:40:09 +0100

    Update Traduction.py

--- pages/Traduction.py
+++ pages/Traduction.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utiles import *
 from mistralai import Mistral
 
-api_keys =st.text_imput("3yXPMZeegB0VuKlWur6DFjdyVRGkzwzz")
+api_keys =st.text_imput("api_keys")
 client = Mistral(api_keys)
 
 prompt = st.test_area("""
